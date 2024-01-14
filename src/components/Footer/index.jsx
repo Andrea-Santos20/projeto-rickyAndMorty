c52b5a57c06a28505944109d6cd56952fccd356b
Andrea <dea0312@gmail.com>
2024-01-14 14:42:06 -0300

    Alguns ajustes nos cards e footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,11 @@ export default function Footer (){
 
     return (
         <>
-            <p className={style.footer}>Para saber mais acesse a <a href="https://rickandmortyapi.com/documentation" target="_blanck">Documentação</a> </p>
+            
+            <p className={style.footer}>Para saber mais acesse a 
+            <img src="/src/components/Footer/book.png" alt="figura de um livro" /> 
+            <a href="https://rickandmortyapi.com/documentation" target="_blanck">Documentação</a>
+            </p>
         </>
     )
 }
