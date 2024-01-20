00395a2fb9c6d8f63786dbb193fb3980afaa5c7e
Andrea <dea0312@gmail.com>
2024-01-20 14:49:37 -0300

    Alterando a tag 'a' para a tag 'link'

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,17 @@
+import { Link } from 'react-router-dom';
 import style from "./Header.module.css";
+
 
 export default function Header (){
 
     return (
         <header>
 
-            <ul>               
-                 
-                <li><a href="#">Home</a></li>                
-                <li><a href="../CardItem/index.jsx">Personagens</a></li>
-                <li><a href="../Footer/index.jsx">Saiba mais</a></li>
+            <ul>          
+                    
+                <li><Link to="/">Início</Link></li>
+                <li><Link to="/personagens">Personagens</Link></li>
+                <li><Link to="/saiba-mais">Saiba mais</Link></li>
                 
             </ul>
 
