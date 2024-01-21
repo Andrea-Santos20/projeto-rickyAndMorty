93ceb31940aa498296db01458becc5cd53433ac5
Andrea <dea0312@gmail.com>
2024-01-21 19:05:31 -0300

    Inserindo icon

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,7 @@
 import style from "./Footer.module.css";
-import { BiBookHeart } from "react-icons/bi";
+//import { BiBookHeart } from "react-icons/bi";
+
+import { FaBook } from "react-icons/fa";
 
 export default function Footer (){
 
@@ -7,7 +9,7 @@ export default function Footer (){
         <>
             
             <p className={style.footer}>Para saber mais acesse a 
-                <BiBookHeart className={style.icon} alt="Figura de um livro azul." />            
+                <FaBook  />            
             <a href="https://rickandmortyapi.com/documentation" target="_blanck">Documentação</a>
             </p>
         </>
